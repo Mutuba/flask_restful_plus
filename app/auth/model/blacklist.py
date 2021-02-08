74ca2e851d906dc69cc0e6035b1741c0933e303d
Mutuba <danielmutubait@gmail.com>
2021-02-08 19:24:59 +0300

    Add changes

--- app/auth/model/blacklist.py
+++ app/auth/model/blacklist.py
@@ -5,18 +5,15 @@ import datetime
 class BlacklistToken(db.Model):
     """
     Token Model for storing JWT tokens
+    
     """
     __tablename__ = 'blacklist_tokens'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     
-    
     token = db.Column(db.String(500), unique=True, nullable=False)
     
-    
     blacklisted_on = db.Column(db.DateTime, nullable=False)
-
-
 
 
     def __init__(self, token):
@@ -31,8 +28,7 @@ class BlacklistToken(db.Model):
         # check whether auth token has been blacklisted
         res = BlacklistToken.query.filter_by(token=str(auth_token)).first()
         
-        if res:
-            
+        if res:         
             return True
         else:
             return False
